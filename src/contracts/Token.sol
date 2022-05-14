1ec65a22b9252b92f5e0bfd70b67659e69927289
idsme <idsmesales@gmail.com>
2022-05-15 00:37:35 +0200

    /feature added transfer method to contract

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,14 +1,49 @@
 pragma solidity ^0.5.0;
 
+// import save match openzepplin library
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
 contract Token{
+    using SafeMath for uint256;
+
     string public name = "IDSME Token" ;
     string public symbol = "IDSME" ;
     uint public decimals = 18;
     uint public totalSupply;
     address public owner;
+    mapping (address => uint256) public balances;
 
+
+    // add initial supply to whoever deploys contract.
     constructor() public {
         totalSupply = 1234567 * (10 ** decimals);
+        balances[msg.sender] = totalSupply;
         owner = msg.sender;
     }
+
+    //transfer tokens to another address
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        //require(_value <= balances[msg.sender]);
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        return true;
+    }
+
+    //return balance of address
+    function balanceOf(address _owner) public view returns (uint balance) {
+        return balances[_owner];
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    // modifier that checks if balance is greater the account balance
+    modifier greaterThanBalance {
+        uint balance = balances[msg.sender];
+        require(balance > msg.value);
+        _;
+    }
+
 }
